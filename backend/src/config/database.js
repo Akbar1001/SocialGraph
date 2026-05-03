c486a34d2546b37c9a240c346856834440a9dfb8
Akbar <akbar.code20@gmail.com>
2026-05-03 14:18:46 +0530

    server setup and database connected

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,13 @@
 const mongoose=require("mongoose");
 
-async function connectToDB(){
+async function connectDB(){
     try{
-        
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to Database Successfully");
 
     }catch(err){
+        console.log("not able to connect with database");
         console.log(err);
     }
 }
+module.exports=connectDB;
